422d85673158394497311bcc58dc84416df194da
Mael FEURGARD <mael.feurgard@laas.fr>
2026-02-15 09:27:05 +0100

    Use a template base implementation for path extension

--- src/ExtendedDubins.hpp
+++ src/ExtendedDubins.hpp
@@ -306,25 +306,101 @@ std::vector<std::unique_ptr<Dubins>> generate_extended_fitted_dubins(
     return output;
 }
 
-// ----- Path generation functions ----- //
-
 /**
- * @brief Generate extended Dubins path from base ones
+ * @brief Generate fitted extended Dubins path using a given generator function
  * 
  * The Extended Dubins paths are made by adding set paths after the start and before the end,
  * thus generating a new Dubins problem which is solved normally. 
  * 
+ * @tparam generator_function A function generating a list of Dubins paths achieving a target length
  * @param start     Start pose
  * @param end       End pose
  * @param start_lens    Lengths for the added initial segment
  * @param end_lens      Lengths for the added final segment
  * @param climb         Climb ratio
  * @param turn_radius   Minimal turn radius
- * @return std::vector<std::unique_ptr<Dubins>>  List of generated paths
+ * @param target_len    Target length
+ * @param tol           Precision for target length
+ * @return std::vector<std::unique_ptr<Dubins>>     List of generated paths
  */
-std::vector<std::unique_ptr<Dubins>> generate_all_extended_from_baseDubins(const Pose3D& start, const Pose3D& end, 
-    std::vector<double> start_lens, std::vector<double> end_lens,
-    double climb, double turn_radius);
+template <Dubins::FittedPathGeneratorFunction generator_function>
+std::vector<std::unique_ptr<Dubins>> generate_all_extended_fitted_dubins(
+    const Pose3D& start, const Pose3D& end, std::vector<double> start_lens, std::vector<double> end_lens,
+    double climb, double turn_radius, double target_len, double tol)
+{
+    std::vector<std::unique_ptr<Dubins>> output,tmp;
+
+    auto unique_ptr_transfer = [&](std::vector<std::unique_ptr<Dubins>>& v)
+    {
+        for(std::unique_ptr<Dubins>& p : v)
+        {
+            output.push_back(std::move(p));
+        }
+    };
+
+    tmp = generate_extended_fitted_dubins<STRAIGHT,STRAIGHT,generator_function>(
+        start,end,start_lens,end_lens,climb,turn_radius,target_len,tol);
+
+    unique_ptr_transfer(tmp);
+
+    tmp = generate_extended_fitted_dubins<STRAIGHT,LEFT,generator_function>(
+        start,end,start_lens,end_lens,climb,turn_radius,target_len,tol);
+
+    unique_ptr_transfer(tmp);
+        
+    tmp = generate_extended_fitted_dubins<STRAIGHT,RIGHT,generator_function>(
+        start,end,start_lens,end_lens,climb,turn_radius,target_len,tol);
+
+    unique_ptr_transfer(tmp);
+    
+
+    
+    tmp = generate_extended_fitted_dubins<LEFT,STRAIGHT,generator_function>(
+        start,end,start_lens,end_lens,climb,turn_radius,target_len,tol);
+
+    unique_ptr_transfer(tmp);
+    
+    tmp = generate_extended_fitted_dubins<LEFT,LEFT,generator_function>(
+        start,end,start_lens,end_lens,climb,turn_radius,target_len,tol);
+
+    unique_ptr_transfer(tmp);
+    
+    tmp = generate_extended_fitted_dubins<LEFT,RIGHT,generator_function>(
+        start,end,start_lens,end_lens,climb,turn_radius,target_len,tol);
+
+    unique_ptr_transfer(tmp);
+
+
+
+    tmp = generate_extended_fitted_dubins<RIGHT,STRAIGHT,generator_function>(
+        start,end,start_lens,end_lens,climb,turn_radius,target_len,tol);
+
+    unique_ptr_transfer(tmp);
+    
+    tmp = generate_extended_fitted_dubins<RIGHT,LEFT,generator_function>(
+        start,end,start_lens,end_lens,climb,turn_radius,target_len,tol);
+
+    unique_ptr_transfer(tmp);
+    
+    tmp = generate_extended_fitted_dubins<RIGHT,RIGHT,generator_function>(
+        start,end,start_lens,end_lens,climb,turn_radius,target_len,tol);
+
+    unique_ptr_transfer(tmp);
+    
+
+    return output;
+}
+
+template <Dubins::FittedPathGeneratorFunction generator_function>
+std::vector<std::unique_ptr<Dubins>> generate_straight_extended_fitted_dubins(
+    const Pose3D& start, const Pose3D& end, std::vector<double> start_lens, std::vector<double> end_lens,
+    double climb, double turn_radius, double target_len, double tol)
+{
+    return generate_extended_fitted_dubins<STRAIGHT,STRAIGHT,generator_function>(
+        start,end,start_lens,end_lens,climb,turn_radius,target_len,tol);
+}
+
+// ----- Path generation functions ----- //
 
 /**
  * @brief Generate fitted extended Dubins path from base ones
